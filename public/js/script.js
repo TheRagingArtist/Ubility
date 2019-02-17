81ef8952bcca7ead754ffa3c6185e940a50868a8
skarmal <skarmal@ncsu.edu>
2019-02-17 03:58:13 -0500

    Adding all files i've been working on

--- public/js/script.js
+++ public/js/script.js
@@ -9,25 +9,132 @@ function getPlaceInfo() {
     urlExtend += word;
     if (word !== words[(words.length - 1)]) {
       urlExtend += "%20";
-      alert("hi1");
     }
   });
   urlExtend += "&inputtype=textquery&fields=photos,formatted_address,name" +
-  "&locationbias=circle:2000@35.910260,-79.055470&key=";
+  "&locationbias=circle:2000@35.7796,-78.6382&key=";
   urlExtend += myApiKey;
-
-    $.ajax({
-        url:'https://jsonplaceholder.typicode.com/todos/1',
+  
+  var name = '';
+  var address = '';
+  var imageUrl = '';
+  
+    var json = $.ajax({
+        url:urlExtend,
         type:"GET",
         success: function(result) {
-            console.log(result);
+            result.candidates.forEach(function(candidate) {
+            
+                name = candidate.name;
+                address = candidate.formatted_address;
+                imageUrl = candidate.photos[0].photo_reference;
+                
+                makeImage(imageUrl);
+                makeName(name);
+                makeAddress(address);
+            
+            });
         }
     });
+}
 
+$('.search-review-button').on('click', function() {
+    getPlaceInfo();
+});
 
-    //onload
-    Http.onreadystatechange=(e)=>{
-      console.log(Http.responseText);
-    }
+function makeImage(photoReference) {
+
+    var url = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=" + photoReference;
+    url += "&key=" + myApiKey;
+    var imgTag = document.createElement("IMG");
+    imgTag.setAttribute("src", url);
+    
+    imgTag.classList.add("grid-image");
+    
+    document.getElementsByClassName('search-results-grid-item grid-1')[0].appendChild(imgTag);
 
 }
+
+function makeName(name) {
+
+    var nameTag = document.createElement("P");
+    
+    var text = document.createTextNode(name);
+    
+    nameTag.appendChild(text);
+    
+    nameTag.classList.add("grid-name");
+    
+    document.getElementsByClassName('search-results-grid-item grid-1')[0].appendChild(nameTag);
+    
+
+}
+
+function makeAddress(address) {
+
+    var addressTag = document.createElement("P");
+    
+    var text = document.createTextNode(address);
+    
+    addressTag.appendChild(text);
+    
+    addressTag.classList.add("grid-address");
+    
+    document.getElementsByClassName('search-results-grid-item grid-1')[0].appendChild(addressTag);
+    
+
+}
+
+const $star1 = $('#1');
+const $star2 = $('#2');
+const $star3 = $('#3');
+const $star4 = $('#4');
+const $star5 = $('#5');
+
+$( '.far' ).click(function() {
+  $( this ).toggleClass( 'fas' );
+});
+
+$star2.on('click', () => {
+    document.getElementById("1").classList.add('fas');
+    document.getElementById("1").style.color = "#15E6DD";
+});
+
+$star3.on('click', () => {
+    document.getElementById("1").classList.add('fas');
+    document.getElementById("1").style.color = "#15E6DD";
+    
+    document.getElementById("2").classList.add('fas');
+    document.getElementById("2").style.color = "#15E6DD";
+});
+
+$star4.on('click', () => {
+    document.getElementById("1").classList.add('fas');
+    document.getElementById("1").style.color = "#15E6DD";
+    
+    document.getElementById("2").classList.add('fas');
+    document.getElementById("2").style.color = "#15E6DD";
+    
+    document.getElementById("3").classList.add('fas');
+    document.getElementById("3").style.color = "#15E6DD";
+});
+
+
+$star5.on('click', () => {
+
+    document.getElementById("1").classList.add('fas');
+    document.getElementById("1").style.color = "#15E6DD";
+    
+    !(if($star1.classList.contains('fas')) {
+        document.getElementById("1").style.color = "white";
+    }
+    
+    document.getElementById("2").classList.add('fas');
+    document.getElementById("2").style.color = "#15E6DD";
+    
+    document.getElementById("3").classList.add('fas');
+    document.getElementById("3").style.color = "#15E6DD";
+    
+    document.getElementById("4").classList.add('fas');
+    document.getElementById("4").style.color = "#15E6DD";
+});
