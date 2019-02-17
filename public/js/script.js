9c7615a99eee8eed4517af61f8e6bac658e4016b
skarmal <skarmal@ncsu.edu>
2019-02-17 08:57:55 -0500

    All js, css html for functionality

--- public/js/script.js
+++ public/js/script.js
@@ -40,6 +40,9 @@ function getPlaceInfo() {
 
 $('.search-review-button').on('click', function() {
     getPlaceInfo();
+    $(".leave-review").removeClass('hidden');
+     $(".review-button").removeClass('hidden');
+    
 });
 
 function makeImage(photoReference) {
@@ -51,7 +54,7 @@ function makeImage(photoReference) {
     
     imgTag.classList.add("grid-image");
     
-    document.getElementsByClassName('search-results-grid-item grid-1')[0].appendChild(imgTag);
+    document.getElementsByClassName('search-results-grid-item')[0].appendChild(imgTag);
 
 }
 
@@ -65,7 +68,7 @@ function makeName(name) {
     
     nameTag.classList.add("grid-name");
     
-    document.getElementsByClassName('search-results-grid-item grid-1')[0].appendChild(nameTag);
+    document.getElementsByClassName('search-results-grid-item')[0].appendChild(nameTag);
     
 
 }
@@ -80,7 +83,7 @@ function makeAddress(address) {
     
     addressTag.classList.add("grid-address");
     
-    document.getElementsByClassName('search-results-grid-item grid-1')[0].appendChild(addressTag);
+    document.getElementsByClassName('search-results-grid-item')[0].appendChild(addressTag);
     
 
 }
@@ -97,44 +100,417 @@ $( '.far' ).click(function() {
 
 $star2.on('click', () => {
     document.getElementById("1").classList.add('fas');
-    document.getElementById("1").style.color = "#15E6DD";
 });
 
 $star3.on('click', () => {
     document.getElementById("1").classList.add('fas');
-    document.getElementById("1").style.color = "#15E6DD";
     
     document.getElementById("2").classList.add('fas');
-    document.getElementById("2").style.color = "#15E6DD";
 });
 
 $star4.on('click', () => {
     document.getElementById("1").classList.add('fas');
-    document.getElementById("1").style.color = "#15E6DD";
     
     document.getElementById("2").classList.add('fas');
-    document.getElementById("2").style.color = "#15E6DD";
     
     document.getElementById("3").classList.add('fas');
-    document.getElementById("3").style.color = "#15E6DD";
 });
 
 
 $star5.on('click', () => {
 
     document.getElementById("1").classList.add('fas');
-    document.getElementById("1").style.color = "#15E6DD";
-    
-    !(if($star1.classList.contains('fas')) {
-        document.getElementById("1").style.color = "white";
-    }
     
     document.getElementById("2").classList.add('fas');
-    document.getElementById("2").style.color = "#15E6DD";
     
     document.getElementById("3").classList.add('fas');
-    document.getElementById("3").style.color = "#15E6DD";
     
     document.getElementById("4").classList.add('fas');
-    document.getElementById("4").style.color = "#15E6DD";
 });
+
+if(document.getElementsByClassName('fa-star')[0].classList.contains("fas")) {
+        document.getElementsByClassName('fa-star')[0].style.color = "#15E6DD";
+}
+
+$( '.accessibility' ).click(function() {
+  $( this ).toggleClass( 'dis' );
+});
+
+const $blind = $('#blind');
+const $deaf = $('#deaf');
+const $sign = $('#sign');
+const $dog = $('#dog');
+const $wheelchair = $('#meh');
+const $crutch= $('#crutch');
+const $lightbulb = $('#lightbulb');
+
+
+if(document.getElementById("blind").classList.contains("dis")) {
+        document.getElementsByClassName('dis')[0].style.color = "#15E6DD";
+}
+
+
+if(document.getElementById("deaf").classList.contains("dis")) {
+        document.getElementsByClassName('dis')[0].style.color = "#15E6DD";
+}
+
+
+if(document.getElementById("sign").classList.contains("dis")) {
+        document.getElementsByClassName('dis')[0].style.color = "#15E6DD";
+}
+
+
+if(document.getElementById("dog").classList.contains("dis")) {
+        document.getElementsByClassName('dis')[0].style.color = "#15E6DD";
+}
+
+
+if(document.getElementById("wheelchair").classList.contains("dis")) {
+        document.getElementsByClassName('dis')[0].style.color = "#15E6DD";
+}
+
+
+if(document.getElementById("crutch").classList.contains("dis")) {
+        document.getElementsByClassName('dis')[0].style.color = "#15E6DD";
+}
+
+
+if(document.getElementById("lightbulb").classList.contains("dis")) {
+        document.getElementsByClassName('dis')[0].style.color = "#15E6DD";
+}
+
+
+$( '.react' ).click(function() {
+  $( this ).toggleClass( 'disa' );
+});
+
+
+
+const $face1 = $('#smile');
+const $face2 = $('#meh');
+const $face3 = $('#frown');
+
+$face1.on('click', () => {
+    document.getElementById("meh").classList.remove('disa');
+    document.getElementById("frown").classList.remove('disa');
+});
+
+$face2.on('click', () => {
+    document.getElementById("smile").classList.remove('disa');
+    document.getElementById("frown").classList.remove('disa');
+});
+
+$face3.on('click', () => {
+    document.getElementById("smile").classList.remove('disa');
+    document.getElementById("meh").classList.remove('disa');
+});
+
+
+if(document.getElementsByClassName('le')[0].classList.contains("disa")) {
+        document.getElementsByClassName('disa')[0].style.color = "#15E6DD";
+}
+
+//modal stuff
+function show_modal_top() {
+  document.getElementsByClassName('modal')[0].style.display = "block";
+}
+function hide_modal_top() {
+  document.getElementsByClassName('modal')[0].style.display = "none";
+}
+
+$('.submit-button').on('click', () => {
+    saveReview();
+    removeRevForm();
+    show_modal_top();
+});
+
+$('.review-button').on('click', () => {
+    show_modal_top();
+});
+
+function saveReview() {
+    var title = document.createElement("H3");
+    var text = document.createTextNode("Reviews");
+    title.appendChild(text);
+    title.classList.add("modal-text-h3");
+    document.getElementsByClassName('modal-content')[0].appendChild(title);
+    
+    var nameTag = document.createElement("P");
+    text = document.createTextNode("Name: " + $('.form-name').val());
+    nameTag.appendChild(text);
+    nameTag.classList.add("modal-text");
+    document.getElementsByClassName('modal-content')[0].appendChild(nameTag);
+    
+    var ageTag = document.createElement("P");
+    text = document.createTextNode("Age: " + $('.form-age').val());
+    ageTag.appendChild(text);
+    ageTag.classList.add("modal-text");
+    document.getElementsByClassName('modal-content')[0].appendChild(ageTag);
+    
+    var disTag = document.createElement("P");
+    text = document.createTextNode("Disability: " + $('.form-dis').val());
+    disTag.appendChild(text);
+    disTag.classList.add("modal-text");
+    document.getElementsByClassName('modal-content')[0].appendChild(disTag);
+    
+    var disTag = document.createElement("DIV");
+    disTag.classList.add("friendly-rating");
+    document.getElementsByClassName('modal-content')[0].appendChild(disTag);
+    
+    
+    var disTag = document.createElement("DIV");
+    disTag.classList.add("accessibility-rating");
+    document.getElementsByClassName('modal-content')[0].appendChild(disTag);
+       
+    if ($("span#smile").hasClass('disa')) { 
+    
+        var friendly = document.createElement("DIV");
+        var friendlyIcon = document.createElement("SPAN");
+        
+        friendly.appendChild(friendlyIcon);
+        
+        friendly.classList.add("friendlyRating");
+        friendlyIcon.classList.add("fas");
+        friendlyIcon.classList.add("fa-grin-beam");
+        friendlyIcon.classList.add("disa");
+        friendlyIcon.classList.add("font-react");
+        
+        document.getElementsByClassName('friendly-rating')[0].appendChild(friendly);
+        
+    } else if ($("span#meh").hasClass('disa')) {
+        
+        var friendly = document.createElement("DIV");
+        var friendlyIcon = document.createElement("SPAN");
+        
+        friendly.appendChild(friendlyIcon);
+        
+        friendly.classList.add("friendlyRating");
+        friendlyIcon.classList.add("fas");
+        friendlyIcon.classList.add("fa-meh");
+        friendlyIcon.classList.add("disa");
+        friendlyIcon.classList.add("font-react");
+        
+        document.getElementsByClassName('friendly-rating')[0].appendChild(friendly);
+        
+    } else if ($("span#frown").hasClass('disa')) {
+        
+        var friendly = document.createElement("DIV");
+        var friendlyIcon = document.createElement("SPAN");
+        
+        friendly.appendChild(friendlyIcon);
+        
+        friendly.classList.add("friendlyRating");
+        friendlyIcon.classList.add("fas");
+        friendlyIcon.classList.add("fa-frown");
+        friendlyIcon.classList.add("disa");
+        friendlyIcon.classList.add("font-react");
+        
+        document.getElementsByClassName('friendly-rating')[0].appendChild(friendly);
+    }
+    if ($("span#blind").hasClass('dis')) {
+        
+        var accessibility = document.createElement("DIV");
+        var accessibilityIcon = document.createElement("SPAN");
+        
+        accessibility.appendChild(accessibilityIcon);
+        
+        accessibility.classList.add("accessibilityRating");
+        accessibilityIcon.classList.add("fas");
+        accessibilityIcon.classList.add("fa-blind");
+        accessibilityIcon.classList.add("dis");
+        accessibilityIcon.classList.add("accessibility");
+        
+        document.getElementsByClassName('accessibility-rating')[0].appendChild(accessibility);
+    }
+    if ($("span#deaf").hasClass('dis')) {
+        
+        var accessibility = document.createElement("DIV");
+        var accessibilityIcon = document.createElement("SPAN");
+        
+        accessibility.appendChild(accessibilityIcon);
+        
+        accessibility.classList.add("accessibilityRating");
+        accessibilityIcon.classList.add("fas");
+        accessibilityIcon.classList.add("fa-deaf");
+        accessibilityIcon.classList.add("dis");
+        accessibilityIcon.classList.add("accessibility");
+        
+        document.getElementsByClassName('accessibility-rating')[0].appendChild(accessibility);
+    }
+    if ($("span#sign").hasClass('dis')) {
+                
+        var accessibility = document.createElement("DIV");
+        var accessibilityIcon = document.createElement("SPAN");
+        
+        accessibility.appendChild(accessibilityIcon);
+        
+        accessibility.classList.add("accessibilityRating");
+        accessibilityIcon.classList.add("fas");
+        accessibilityIcon.classList.add("fa-sign-language");
+        accessibilityIcon.classList.add("dis");
+        accessibilityIcon.classList.add("accessibility");
+        
+        document.getElementsByClassName('accessibility-rating')[0].appendChild(accessibility);
+    }
+    if ($("span#dog").hasClass('dis')) {
+                
+        var accessibility = document.createElement("DIV");
+        var accessibilityIcon = document.createElement("SPAN");
+        
+        accessibility.appendChild(accessibilityIcon);
+        
+        accessibility.classList.add("accessibilityRating");
+        accessibilityIcon.classList.add("fas");
+        accessibilityIcon.classList.add("fa-dog");
+        accessibilityIcon.classList.add("dis");
+        accessibilityIcon.classList.add("accessibility");
+        
+        document.getElementsByClassName('accessibility-rating')[0].appendChild(accessibility);
+    }
+    if ($("span#wheelchair").hasClass('dis')) {
+                       
+        var accessibility = document.createElement("DIV");
+        var accessibilityIcon = document.createElement("SPAN");
+        
+        accessibility.appendChild(accessibilityIcon);
+        
+        accessibility.classList.add("accessibilityRating");
+        accessibilityIcon.classList.add("fas");
+        accessibilityIcon.classList.add("fa-wheelchair");
+        accessibilityIcon.classList.add("dis");
+        accessibilityIcon.classList.add("accessibility");
+        
+        document.getElementsByClassName('accessibility-rating')[0].appendChild(accessibility);
+    }
+    if ($("span#crutch").hasClass('dis')) {
+        
+         var accessibility = document.createElement("DIV");
+        var accessibilityIcon = document.createElement("SPAN");
+        
+        accessibility.appendChild(accessibilityIcon);
+        
+        accessibility.classList.add("accessibilityRating");
+        accessibilityIcon.classList.add("fas");
+        accessibilityIcon.classList.add("fa-crutch");
+        accessibilityIcon.classList.add("dis");
+        accessibilityIcon.classList.add("accessibility");
+        
+        document.getElementsByClassName('accessibility-rating')[0].appendChild(accessibility);
+    }
+    if ($("span#lightbulb").hasClass('dis')) {
+        
+        var accessibility = document.createElement("DIV");
+        var accessibilityIcon = document.createElement("SPAN");
+        
+        accessibility.appendChild(accessibilityIcon);
+        
+        accessibility.classList.add("accessibilityRating");
+        accessibilityIcon.classList.add("fas");
+        accessibilityIcon.classList.add("fa-lightbulb");
+        accessibilityIcon.classList.add("dis");
+        accessibilityIcon.classList.add("accessibility");
+        
+        document.getElementsByClassName('accessibility-rating')[0].appendChild(accessibility);
+    }
+    
+    
+    if ($("span#1").hasClass('fas')) {
+        
+        var ratingOuter1 = document.createElement("DIV");
+        var rating = document.createElement("DIV");
+        var ratingIcon = document.createElement("SPAN");
+        
+        
+        rating.appendChild(ratingIcon);
+        ratingOuter1.appendChild(rating);
+        
+        ratingOuter1.classList.add("ratingOuter1");
+        rating.classList.add("rating");
+        ratingIcon.classList.add("fas");
+        ratingIcon.classList.add("fa-star");
+        ratingIcon.classList.add("far");
+        
+        document.getElementsByClassName('modal-content')[0].appendChild(ratingOuter1);
+    }
+    if ($("span#2").hasClass('fas')) {
+        
+        var ratingOuter2 = document.createElement("DIV");
+        var rating = document.createElement("DIV");
+        var ratingIcon = document.createElement("SPAN");
+        
+        
+        rating.appendChild(ratingIcon);
+        ratingOuter2.appendChild(rating);
+        
+        ratingOuter2.classList.add("ratingOuter2");
+        rating.classList.add("rating");
+        ratingIcon.classList.add("fas");
+        ratingIcon.classList.add("fa-star");
+        ratingIcon.classList.add("far");
+        
+        document.getElementsByClassName('modal-content')[0].appendChild(ratingOuter2);
+    }
+    if ($("span#3").hasClass('fas')) {
+        
+        var ratingOuter3 = document.createElement("DIV");
+        var rating = document.createElement("DIV");
+        var ratingIcon = document.createElement("SPAN");
+        
+        
+        rating.appendChild(ratingIcon);
+        ratingOuter3.appendChild(rating);
+        
+        ratingOuter3.classList.add("ratingOuter3");
+        rating.classList.add("rating");
+        ratingIcon.classList.add("fas");
+        ratingIcon.classList.add("fa-star");
+        ratingIcon.classList.add("far");
+        
+        document.getElementsByClassName('modal-content')[0].appendChild(ratingOuter3);
+    }
+    if ($("span#4").hasClass('fas')) {
+        
+        var ratingOuter4 = document.createElement("DIV");
+        var rating = document.createElement("DIV");
+        var ratingIcon = document.createElement("SPAN");
+        
+        
+        rating.appendChild(ratingIcon);
+        ratingOuter4.appendChild(rating);
+        
+        ratingOuter4.classList.add("ratingOuter4");
+        rating.classList.add("rating");
+        ratingIcon.classList.add("fas");
+        ratingIcon.classList.add("fa-star");
+        ratingIcon.classList.add("far");
+        
+        document.getElementsByClassName('modal-content')[0].appendChild(ratingOuter4);
+    }
+    if ($("span#5").hasClass('fas')) {
+        
+        var ratingOuter5 = document.createElement("DIV");
+        var rating = document.createElement("DIV");
+        var ratingIcon = document.createElement("SPAN");
+        
+        
+        rating.appendChild(ratingIcon);
+        ratingOuter5.appendChild(rating);
+        
+        ratingOuter5.classList.add("ratingOuter5");
+        rating.classList.add("rating");
+        ratingIcon.classList.add("fas");
+        ratingIcon.classList.add("fa-star");
+        ratingIcon.classList.add("far");
+        
+        document.getElementsByClassName('modal-content')[0].appendChild(ratingOuter5);
+    }
+    var revTag = document.createElement("P");
+    text = document.createTextNode("Review: " + $('.form-rev').val());
+    revTag.appendChild(text);
+    revTag.classList.add("modal-text");
+    document.getElementsByClassName('modal-content')[0].appendChild(revTag);
+}
+function removeRevForm() {
+    $('.leave-review').remove();
+    $('.ubility-leave-review-container').remove();
+}
